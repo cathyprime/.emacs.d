304a9925746788298ddfa54d4dd877bf4b48e856
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-05 23:50:29 +0100

    feat: scala setup numerous fixes for lsp mode and add hooks

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company lsp-mode treemacs-evil treemacs highlight-indent-guides markdown-mode python-mode go-mode which-key wgrep vterm undo-tree smex magit ligature evil-surround evil-commentary evil-collection autothemer)))
+   '(lsp-metals lsp-pyright company lsp-mode treemacs-evil treemacs highlight-indent-guides markdown-mode python-mode go-mode which-key wgrep vterm undo-tree smex magit ligature evil-surround evil-commentary evil-collection autothemer)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -154,17 +154,58 @@
 (use-package company
   :ensure t
   :config
+  :hook ((emacs-lisp-mode . (lambda ()
+							  (setq-local company-backends '(company-elisp))))
+		 (emacs-lisp-mode . company-mode))
   (company-keymap--unbind-quick-access company-active-map)
   (setq company-idle-delay 0.1
 		company-minimum-prefix-length 1))
 
-;; language stuff
+;; language specific stuff
 (use-package go-mode
-  :ensure t)
+  :ensure t
+  :hook ((go-mode . lsp-deferred)
+		 (go-mode . company-mode))
+  :bind (:map go-mode-map
+			  ("C-c f" . gofmt))
+  :config
+  (require 'lsp-go)
+  (setq lsp-go-analyses
+		'((fieldalignment . t)
+		  (nilness        . t)
+		  (unusedwrite    . t)
+		  (unusedparams   . t)))
+  (add-to-list 'exec-path "~/go/bin")
+  (setq gofmt-command "goimports"))
 
 (use-package python-mode
   :ensure t
   :mode (("\\.py\\'" . python-ts-mode)))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp-deferred))))
+
+(use-package lsp-metals
+  :ensure t
+  :custom
+  ;; You might set metals server options via -J arguments. This might not always work, for instance when
+  ;; metals is installed using nix. In this case you can use JAVA_TOOL_OPTIONS environment variable.
+  (lsp-metals-server-args '(;; Metals claims to support range formatting by default but it supports range
+                            ;; formatting of multiline strings only. You might want to disable it so that
+                            ;; emacs can use indentation provided by scala-mode.
+                            "-J-Dmetals.allow-multiline-string-formatting=off"
+                            ;; Enable unicode icons. But be warned that emacs might not render unicode
+                            ;; correctly in all cases.
+                            "-J-Dmetals.icons=unicode"))
+  ;; In case you want semantic highlighting. This also has to be enabled in lsp-mode using
+  ;; `lsp-semantic-tokens-enable' variable. Also you might want to disable highlighting of modifiers
+  ;; setting `lsp-semantic-tokens-apply-modifiers' to `nil' because metals sends `abstract' modifier
+  ;; which is mapped to `keyword' face.
+  (lsp-metals-enable-semantic-highlighting t)
+  :hook (scala-mode . lsp))
 
 (use-package highlight-indent-guides
   :ensure t
