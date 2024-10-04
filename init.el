6e94960fc5c90a26e15fbaf00b28bdc4450ac92a
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-04 12:49:46 +0200

    feat: clean up the config

--- init.el
+++ init.el
@@ -1,34 +1,12 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(elcord sly spotify haskell-mode racket-mode multiple-cursors lua-mode company highlight-indent-guides markdown-mode python-mode go-mode which-key wgrep vterm smex magit ligature autothemer)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
 
-(defun magda/set-font-faces ()
-  (set-face-font 'default "JetBrainsMono Nerd Font-16")
-  (load-theme 'kanagawa t)
-  (blink-cursor-mode))
-
-(magda/set-font-faces)
-
+(set-face-font 'default "Iosevka Nerd Font-18")
+(blink-cursor-mode)
 (setq make-backup-files nil)
 
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-			  (lambda (frame)
-				(with-selected-frame frame
-				  (magda/set-font-faces))))
-  (magda/set-font-faces))
-
 (global-set-key (kbd "M-&") 'with-editor-async-shell-command)
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
 
 (setq default-frame-alist '((undecorated . t)))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -38,6 +16,7 @@
 (setq split-width-threshold nil)
 (setq isearch-lazy-count t)
 (setq lazy-count-prefix-format "(%s/%s) ")
+(setq inhibit-startup-screen t)
 (setq search-whitespace-regexp ".*?")
 (setq-default tab-width 4)
 (blink-cursor-mode 0)
@@ -49,35 +28,9 @@
 (require 'package)
 (require 'use-package)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+			 '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
-
-(use-package ligature
-  :load-path "path-to-ligature-repo"
-  :config
-  ;; Enable the "www" ligature in every possible major mode
-  (ligature-set-ligatures 't '("www"))
-  ;; Enable traditional ligature support in eww-mode, if the
-  ;; `variable-pitch' face supports it
-  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-  ;; Enable all Cascadia Code ligatures in programming modes
-  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
-                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
-                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
-                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
-                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
-                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
-                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
-                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
-                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
-                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
-                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
-                                       "\\\\" "://"))
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
 
 (use-package vterm
   :ensure t)
@@ -91,9 +44,11 @@
   :ensure t
   :config
   (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+  (global-set-key (kbd "C->")         'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+  (global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+  (global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this))
  
 (use-package ido
   :config
@@ -110,35 +65,3 @@
 
 (use-package wgrep
   :ensure t)
-
-(use-package company
-  :ensure t
-  :config
-  :hook ((emacs-lisp-mode . (lambda ()
-							  (setq-local company-backends '(company-elisp))))
-		 (emacs-lisp-mode . company-mode))
-  (company-keymap--unbind-quick-access company-active-map)
-  (setq company-idle-delay 0.1
-		company-minimum-prefix-length 1))
-
-;; language specific stuff
-(use-package go-mode
-  :ensure t
-  :hook ((go-mode . lsp-deferred)
-		 (go-mode . company-mode))
-  :bind (:map go-mode-map
-			  ("C-c f" . gofmt))
-  :config
-  (add-to-list 'exec-path "~/go/bin")
-  (setq gofmt-command "goimports"))
-
-(use-package highlight-indent-guides
-  :ensure t
-  :hook (python-ts-mode . highlight-indent-guides-mode)
-  :config
-  (setq highlight-indent-guides-method 'character))
-
-(use-package markdown-mode
-  :ensure t
-  :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
