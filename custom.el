5187112dc557d1f8eaeb6a0d03d07e34ef338ad4
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-06 22:17:23 +0200

    feat: save custom vars

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-operandi-tinted))
+ '(custom-safe-themes
+   '("99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e" default))
+ '(package-selected-packages '(wgrep modus-themes magit smex multiple-cursors which-key)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
