bd4aa6725be5cbfbb980d6d4703a6abf066cae25
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-07 18:55:23 +0200

    feat: supress emacs errors

--- custom.el
+++ custom.el
@@ -6,7 +6,8 @@
  '(custom-enabled-themes '(modus-operandi-tinted))
  '(custom-safe-themes
    '("99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e" default))
- '(package-selected-packages '(wgrep modus-themes magit smex multiple-cursors which-key)))
+ '(package-selected-packages '(wgrep modus-themes magit smex multiple-cursors which-key))
+ '(warning-minimum-level :emergency))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
