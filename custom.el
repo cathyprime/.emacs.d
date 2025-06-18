ba5885bcb406eb47fc380c8f230dffca7df1d09e
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-06-18 05:33:52 +0200

    use meow instead of god-mode + change-inner, remove pdf-tools

--- custom.el
+++ custom.el
@@ -5,9 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-operandi-tinted))
  '(custom-safe-themes
-   '("99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e" default))
+   '("9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
+	 "99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e"
+	 default))
  '(package-selected-packages
-   '(rfc-mode ess-R-data-view essgd auctex change-inner pdf-tools god-mode rust-mode lua-mode nov ess move-text ivy wgrep modus-themes magit smex multiple-cursors which-key))
+   '(a68-mode auctex change-inner ess ess-R-data-view essgd god-mode ivy
+			  lua-mode magit meow modus-themes move-text
+			  multiple-cursors nov php-mode rfc-mode rust-mode smex
+			  wgrep which-key))
  '(warning-minimum-level :emergency))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init.el
+++ init.el
@@ -108,7 +108,9 @@
   :ensure t
   :config
   (global-set-key (kbd "M-p") 'move-text-up)
-  (global-set-key (kbd "M-n") 'move-text-down))
+  (global-set-key (kbd "M-<up>") 'move-text-up)
+  (global-set-key (kbd "M-n") 'move-text-down)
+  (global-set-key (kbd "M-<down>") 'move-text-down))
 
 (use-package ess
   :ensure t)
@@ -119,26 +121,94 @@
 (use-package nov
   :ensure t)
 
-(use-package god-mode
-  :ensure t
-  :init (setq god-mode-enable-function-key-translation nil)
-  :config
-  (global-set-key (kbd "<escape>") #'god-local-mode)
-  (define-key god-local-mode-map (kbd ".") #'repeat))
-
-(use-package pdf-tools
-  :ensure t
-  :config
-  (pdf-tools-install))
-
-(use-package change-inner
-  :ensure t
-  :config
-  (global-set-key (kbd "M-i") 'change-inner)
-  (global-set-key (kbd "M-o") 'change-outer)
-  (global-set-key (kbd "C-=") 'er/expand-region))
-
-(use-package auctex
+(use-package meow
   :ensure t)
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(require 'meow)
+(meow-setup)
+(meow-global-mode 1)
 
 (load-file custom-file)
+(put 'narrow-to-region 'disabled nil)
