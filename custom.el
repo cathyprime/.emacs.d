64979ad976f41ccfee053b0b3b27919f3b6cac0e
Magdalena <magdalena.mjaroszynska@gmail.com>
2026-01-21 11:20:36 +0100

    Make some changes to make it easier to work with default mappings

--- custom.el
+++ custom.el
@@ -8,9 +8,9 @@
    '("9113a2a0e6f13b8fe851c6c5a9b2a1a9608b9aae28b411c81211315b2e312007"
 	 default))
  '(package-selected-packages
-   '(consult copilot copilot-chat esxml ivy magit marginalia meow
-			 modus-themes move-text multiple-cursors orderless smex
-			 vertico wgrep)))
+   '(consult esxml ivy magit marginalia modus-themes move-text
+			 multiple-cursors objed orderless vertico wgrep
+			 wrap-region)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
